f799024eab53e6d3eddbd450fbbf82ac5a44ee56
Adriane Hanson <ahansonuga@gmail.com>
2024-01-08 17:18:48 -0500

    vectorize and cluster

--- topic-modeling-2.py
+++ topic-modeling-2.py
@@ -75,5 +75,42 @@ names = load_data("data/trc_dn.json")["names"]
 
 # Clean up the descriptions.
 cleaned_docs = clean_docs(descriptions)
-print(descriptions[0])
-print(cleaned_docs[0])
+
+# Get key words for each document.
+# Standard code from other sources. Can customize in different ways.
+# max_df and min_df are the range of words to include; anything outside is too common or rare and excluded.
+# ngram = the number of words to consider as a phrase (bigram, trigram, etc.)
+vectorizer = TfidfVectorizer(lowercase=True, max_features=100, max_df=0.8, min_df=5, ngram_range=(1, 3), stop_words="english")
+vectors = vectorizer.fit_transform(cleaned_docs)
+feature_names = vectorizer.get_feature_names_out()
+dense = vectors.todense()
+denselist = dense.tolist()
+all_keywords = []
+for description in denselist:
+    x = 0
+    keywords = []
+    for word in description:
+        if word > 0:
+            keywords.append(feature_names[x])
+        x = x + 1
+    all_keywords.append(keywords)
+
+# Cluster key words from each document to see where there is overlap.
+# true_k is the number of clusters
+true_k = 20
+model = KMeans(n_clusters=true_k, init="k-means++", max_iter=100, n_init=1)
+model.fit(vectors)
+order_centroids = model.cluster_centers_.argsort()[:, ::-1]
+terms = vectorizer.get_feature_names_out()
+
+# Save the clusters to a text file in the repo folder.
+with open("data/trc_results.txt", "w", encoding="utf-8") as f:
+    for i in range(true_k):
+        f.write(f"Cluster {i}")
+        f.write("\n")
+        for ind in order_centroids[i, :10]:
+            f.write(' %s' % terms[ind],)
+            f.write("\n")
+        f.write("\n")
+        f.write("\n")
+
